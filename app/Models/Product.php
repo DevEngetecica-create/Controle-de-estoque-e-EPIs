ac974807c91fa7e8e7d4f48f83eeff6d323c15e7
Samuel Ferreira de Melo <samueltgq@gmail.com>
2024-07-05 14:52:00 -0300

    Commit: Criação das models produto, categorias, subcategorias e marcas.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // app/Models/Product.php
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    
 }
