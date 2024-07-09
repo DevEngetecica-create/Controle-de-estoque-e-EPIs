ba2001ce79a1066ac18a3457e955767b519b00ee
Samuel Ferreira de Melo <samueltgq@gmail.com>
2024-07-09 16:51:10 -0300

    Commit: Criado as views create, edit e show das categorias

--- resources/views/products/categories/create.blade.php
+++ resources/views/products/categories/create.blade.php
@@ -5,49 +5,19 @@
     <div class="card">
         <div class="card-body p-5">
             <h1>Cadastrar Produto</h1>
-            <form action="{{ route('products.store') }}" method="POST">
+            <form action="{{ route('categories.store') }}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="name">Nome</label>
                     <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
                 </div>
                 <div class="form-group">
-                    <label for="quantity">Quantidade</label>
-                    <input type="number" class="form-control" id="quantity" name="quantity" value="{{ old('quantity') }}">
+                    <label for="quantity">Cor</label>
+                    <input type="text" class="form-control" id="colorInput" name="color" value="{{ old('color') }}">
                 </div>
-                <div class="form-group">
-                    <label for="unit_price">Preço Unitário</label>
-                    <input type="text" class="form-control" id="unit_price" name="unit_price" value="{{ old('unit_price') }}">
-                </div>
-                <div class="form-group">
-                    <label for="expiry_date">Data de Validade</label>
-                    <input type="date" class="form-control" id="expiry_date" name="expiry_date" value="{{ old('expiry_date') }}">
-                </div>
-                <div class="form-group">
-                    <label for="category_id">Categoria</label>
-                    <select class="form-control" id="category_id" name="category_id">
-                        @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="subcategory_id">Subcategoria</label>
-                    <select class="form-control" id="subcategory_id" name="subcategory_id">
-                        @foreach($subcategories as $subcategory)
-                        <option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="brand_id">Marca</label>
-                    <select class="form-control" id="brand_id" name="brand_id">
-                        @foreach($brands as $brand)
-                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <!-- Outros campos aqui -->
+
+                <div id="colorDiv" style="width: 150px; height:150px; background-color: #ffff2; margin: 15px"></div>
+
                 <button type="submit" class="btn btn-primary">Salvar</button>
             </form>
         </div>
